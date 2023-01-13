463c004d0f35c98e868d174006747c153aae641e
dennis mwaniki <dennomaz@gmail.com>
2023-01-13 13:36:43 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
-
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+import MesaVerde from "./parks/MesaVerde";
+import {trees, elevation, wildlife} from "./parks/RockyMountain"
+ReactDOM.render(
+    <>
+        <ColoradoStateParks />
+        <MesaVerde />
+        <div>
+        {trees}
+        {elevation()}
+        {wildlife()}
+        </div>
+    </>,
+    document.getElementById("root")
+    );
